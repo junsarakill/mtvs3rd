a3a549a4e377afa8d1b98aa28ecb51dbd12ca7ea
junsarakill <61420467+junsarakill@users.noreply.github.com>
2024-09-05 19:22:05 +0900

    .

---
+++ Source/mtvs3rd/Private/JBS/BS_PlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JBS/BS_PlayerState.h"
+
+void ABS_PlayerState::SetPlayerData(FPSH_HttpDataTable data)
+{
+    playerData = data;
+}
