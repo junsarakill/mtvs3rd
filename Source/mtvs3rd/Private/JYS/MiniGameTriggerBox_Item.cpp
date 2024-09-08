bd60f4310d5eee4b6edd2e99ed6382d8d25ec1b7
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-09-08 23:34:54 +0900

    Feat: 액터의 태그가 같으면 벽 FadeOut

---
+++ Source/mtvs3rd/Private/JYS/MiniGameTriggerBox_Item.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JYS/MiniGameTriggerBox_Item.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "JYS/MiniGameWall.h"
+
+AMiniGameTriggerBox_Item::AMiniGameTriggerBox_Item()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	itemBox1 = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox1"));
+	itemBox1->SetupAttachment(RootComponent);
+	itemBox1->SetCollisionProfileName("OverlapAllDynamic");
+
+	itemBox2 = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox2"));
+	itemBox2->SetupAttachment(RootComponent);
+	itemBox2->SetCollisionProfileName("OverlapAllDynamic");
+
+	itemBox1->OnComponentBeginOverlap.AddDynamic(this, &AMiniGameTriggerBox_Item::OnOverlapBegin1);
+	itemBox2->OnComponentBeginOverlap.AddDynamic(this, &AMiniGameTriggerBox_Item::OnOverlapBegin2);
+
+}
+
+void AMiniGameTriggerBox_Item::BeginPlay()
+{
+}
+
+void AMiniGameTriggerBox_Item::Tick(float DeltaTime)
+{
+}
+
+void AMiniGameTriggerBox_Item::OnOverlapBegin1(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	CurrentActor1 = OtherActor;
+	CheckTags();
+}
+
+void AMiniGameTriggerBox_Item::OnOverlapBegin2(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	CurrentActor2 = OtherActor;
+	CheckTags();
+}
+
+void AMiniGameTriggerBox_Item::CheckTags()
+{
+	TArray<AActor*> tag;
+
+	if (CurrentActor1->Tags[0] == CurrentActor2->Tags[0])
+	{
+		UGameplayStatics::GetAllActorsWithTag(GetWorld(), TEXT("fadeOut"), tag);
+		AMiniGameWall* fadeOutWall = Cast<AMiniGameWall>(tag[0]);
+		fadeOutWall->SetFadeOut();
+	}
+}
