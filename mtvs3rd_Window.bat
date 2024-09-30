9f047be5bd2bb703dadab04c32fc8d1f3faa8266
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-09-30 10:46:53 +0900

    debug
    debug

--- mtvs3rd_Window.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\Admin\Documents\GitHub\mtvs3rd\mtvs3rd.uproject" -game -ResX=640 -ResY=480 -WinX=200 -WinY=300 -log
